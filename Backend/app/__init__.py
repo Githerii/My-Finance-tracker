1935daea8356b57eee3a8c12a8dbb84b323dbb50
Githerii <kuiranathan@gmail.com>
2026-01-30 21:36:55 +0300

    The Flask factory

---
+++ Backend/app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+import os
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "sqlite:///" + os.path.join(BASE_DIR, "finance.db")
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.config["JWT_SECRET_KEY"] = os.getenv(
+        "JWT_SECRET_KEY", "dev-secret-change-me"
+    )
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from .app import register_routes
+    register_routes(app)
+
+    return app
