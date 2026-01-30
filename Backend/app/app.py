1059a0b0548caa08ad10dc498a8c94e730a430a2
Githerii <kuiranathan@gmail.com>
2026-01-30 22:10:41 +0300

    Creation of login endpoint with JWT  implemented

--- Backend/app/app.py
+++ Backend/app/app.py
@@ -43,3 +43,29 @@ def register_routes(app):
         db.session.commit()
 
         return jsonify({"message": "User registered successfully"}), 201
+
+
+    @app.route("/auth/login", methods=["POST"])
+    def login():
+        data = request.get_json()
+
+        email = data.get("email")
+        password = data.get("password")
+
+        if not email or not password:
+            return jsonify({"error": "Email and password are required"}), 400
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user or not check_password_hash(user.password_hash, password):
+            return jsonify({"error": "Invalid credentials"}), 401
+
+        # Create JWT
+        access_token = create_access_token(
+            identity=user.id,
+            expires_delta=timedelta(days=1)
+        )
+
+        return jsonify({
+            "access_token": access_token
+        }), 200
