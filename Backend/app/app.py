2d3fcc25606a9c302598348659f65afff5a88ae8
Githerii <kuiranathan@gmail.com>
2026-01-30 22:08:03 +0300

    User creation route

--- Backend/app/app.py
+++ Backend/app/app.py
@@ -1,5 +1,10 @@
-from flask import jsonify
+from flask import jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
 
+from .models import User
+from . import db
 
 def register_routes(app):
 
@@ -9,3 +14,32 @@ def register_routes(app):
             "status": "ok",
             "message": "Finance Tracker API running"
         })
+
+    @app.route("/auth/register", methods=["POST"])
+    def register():
+        data = request.get_json()
+
+        email = data.get("email")
+        password = data.get("password")
+
+        # Basic validation
+        if not email or not password:
+            return jsonify({"error": "Email and password are required"}), 400
+
+        # Checks if user exists
+        if User.query.filter_by(email=email).first():
+            return jsonify({"error": "User already exists"}), 409
+
+        # Hashing the password
+        password_hash = generate_password_hash(password)
+
+        # Creates a user
+        user = User(
+            email=email,
+            password_hash=password_hash
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"message": "User registered successfully"}), 201
